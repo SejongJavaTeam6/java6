099a169f2e773b2336917a578f06acb8808004c1
lulyulalla <daye200@naver.com>
2024-11-27 18:39:32 +0900

    build(초기세팅):초기세팅

---
+++ javaTeam6/src/main/java/com/example/javateam6/JavaTeam6Application.java
@@ -0,0 +1,13 @@
+package com.example.javateam6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTeam6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaTeam6Application.class, args);
+	}
+
+}
---
+++ javaTeam6/src/test/java/com/example/javateam6/JavaTeam6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javateam6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaTeam6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
